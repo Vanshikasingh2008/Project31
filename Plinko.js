14a78bd171d30599e775653904bc31a5edf19b79
Vanshikasingh2008 <64592848+Vanshikasingh2008@users.noreply.github.com>
2020-07-05 13:15:08 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -6,8 +6,10 @@ class Plinko{
         this.body = Bodies.circle(x,y,10,op);
         World.add(world,this.body);
     }
+
     display(){
         push();
+        fill("white")
         ellipseMode(RADIUS);
         ellipse(this.body.position.x,this.body.position.y,10);
         pop();
